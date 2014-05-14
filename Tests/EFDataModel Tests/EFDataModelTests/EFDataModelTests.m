5406cc92cd1707d8b7d8df0d6bf098131de1e40d
Elton Faggett <eltonf@290design.com>
2014-05-13 23:54:07 -0500

    Created a tests project

---
+++ Tests/EFDataModel Tests/EFDataModelTests/EFDataModelTests.m
@@ -0,0 +1,34 @@
+//
+//  EFDataModelTests.m
+//  EFDataModelTests
+//
+//  Created by Elton Faggett on 5/13/14.
+//  Copyright (c) 2014 290 Design, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EFDataModelTests : XCTestCase
+
+@end
+
+@implementation EFDataModelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
